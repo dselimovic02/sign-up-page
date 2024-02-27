7aa197e738fd0dcff2b91eccde7292306ac1b4fa
dselimovic <dselimovic02@gmail.com>
2024-02-27 16:34:25 +0100

    Add styles and form functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+const fields = document.querySelectorAll(".pwd");
+const note = document.querySelector(".pwd-note");
+
+fields.forEach(element => {
+  element.addEventListener("keyup", () => {
+    console.log(fields[0].value);
+    console.log(fields[1].value);
+    if (fields[0].value != fields[1].value) {
+      fields[0].classList.add("error");
+      fields[1].classList.add("error");
+      note.style.display = "block";
+    } else {
+      fields[0].classList.remove("error");
+      fields[1].classList.remove("error");
+      note.style.display = "none";
+    }
+  });
+});
+
